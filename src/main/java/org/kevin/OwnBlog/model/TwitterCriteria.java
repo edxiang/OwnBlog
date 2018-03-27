7503bcda131bb8880964a7aebd129bc276b40f3f
edxiao <229270808@qq.com>
2018-03-27 16:35:57 +0800

    changge sth.

--- src/main/java/org/kevin/OwnBlog/model/TwitterCriteria.java
+++ src/main/java/org/kevin/OwnBlog/model/TwitterCriteria.java
@@ -2,11 +2,13 @@ package org.kevin.OwnBlog.model;
 
 import lombok.Data;
 
+import java.util.Date;
+
 /**
  * Created by Kevin.Z on 2017/12/13.
  */
 @Data
-public class AlbumCriteria {
-    private String tag;
-    private String title;
+public class TwitterCriteria {
+    private Date fromDate;
+    private Date toDate;
 }
--- src/main/java/org/kevin/OwnBlog/service/TwitterService.java
+++ src/main/java/org/kevin/OwnBlog/service/TwitterService.java
@@ -2,9 +2,21 @@ package org.kevin.OwnBlog.service;
 
 import org.kevin.OwnBlog.dao.TwitterRepository;
 import org.kevin.OwnBlog.model.Twitter;
+import org.kevin.OwnBlog.model.TwitterCriteria;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -29,5 +41,28 @@ public class TwitterService {
 
     public void delete(Twitter twitter){
         twitterRepository.delete(twitter);
+    }
+
+    public long count(){
+        return twitterRepository.count();
+    }
+
+    public Page<Twitter> findTwitterCriteria(int page, int size, final TwitterCriteria twitterCriteria){
+        Pageable pageable = new PageRequest(page,size, Sort.Direction.ASC,"id");
+        Page<Twitter> twitterPage = twitterRepository.findAll(new Specification<Twitter>() {
+            @Override
+            public Predicate toPredicate(Root<Twitter> root, CriteriaQuery<?> criteriaQuery, CriteriaBuilder criteriaBuilder) {
+                List<Predicate> list = new ArrayList<>();
+                if(twitterCriteria.getFromDate() != null){
+                    list.add(criteriaBuilder.equal(root.get("createTime").as(Date.class), twitterCriteria.getFromDate()));
+                }
+                if(twitterCriteria.getToDate() != null){
+                    list.add(criteriaBuilder.equal(root.get("createTime").as(Date.class), twitterCriteria.getToDate()));
+                }
+                Predicate[] p = new Predicate[list.size()];
+                return criteriaBuilder.and(list.toArray(p));
+            }
+        }, pageable);
+        return twitterPage;
     }
 }
