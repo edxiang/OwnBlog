70dd7638f99e01f87a1333cbf0e335cb1e2357a2
edxiao <229270808@qq.com>
2018-03-27 16:10:32 +0800

    add the pagination

---
+++ src/main/java/org/kevin/OwnBlog/model/TwitterCriteria.java
@@ -0,0 +1,12 @@
+package org.kevin.OwnBlog.model;
+
+import lombok.Data;
+
+/**
+ * Created by Kevin.Z on 2017/12/13.
+ */
+@Data
+public class AlbumCriteria {
+    private String tag;
+    private String title;
+}
