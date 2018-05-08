9147b3120516ceb3ff8b528b1a181d870adf83b2
edxiao <229270808@qq.com>
2018-05-08 11:56:02 +0800

    adjust the page

--- src/main/java/org/kevin/OwnBlog/controller/DiaryController.java
+++ src/main/java/org/kevin/OwnBlog/controller/DiaryController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Collections;
@@ -73,5 +74,12 @@ public class DiaryController {
             diaryService.save(diary);
         }
         return "redirect:../diary";
+    }
+
+    @RequestMapping(value = "/diary/changeSentence", method = RequestMethod.GET)
+    @ResponseBody
+    public String changeSentence(HttpServletRequest request){
+        String s = request.getParameter("newSentence");
+        return s;
     }
 }
